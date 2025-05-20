fc99760d3ed40580db6f280ed273757e57f95914
OuterCyrex <a15839962056@outlook.com>
2025-05-20 22:38:51 +0800

    🎇 feat: project initialized

---
+++ src/components/home/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-menu
+    mode="horizontal"
+  >
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Navigation from "@/components/home/Navigation.vue";
+</script>
+
+<template>
+  <Navigation />
+  <el-row>
+
+  </el-row>
+</template>
